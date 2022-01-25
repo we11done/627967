5e419f87b7ad55f1127569cf428b52b29f196313
Jaehyun Jun <wjswogus0007@gmail.com>
2022-01-25 18:10:28 -0500

    fix: postMessage function now awaits for the saveMessage response

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -1,29 +1,29 @@
-import axios from "axios";
-import socket from "../../socket";
+import axios from 'axios';
+import socket from '../../socket';
 import {
   gotConversations,
   addConversation,
   setNewMessage,
   setSearchedUsers,
-} from "../conversations";
-import { gotUser, setFetchingStatus } from "../user";
+} from '../conversations';
+import { gotUser, setFetchingStatus } from '../user';
 
 axios.interceptors.request.use(async function (config) {
-  const token = await localStorage.getItem("messenger-token");
-  config.headers["x-access-token"] = token;
+  const token = await localStorage.getItem('messenger-token');
+  config.headers['x-access-token'] = token;
 
   return config;
 });
 
 // USER THUNK CREATORS
 
-export const fetchUser = () => async (dispatch) => {
+export const fetchUser = () => async dispatch => {
   dispatch(setFetchingStatus(true));
   try {
-    const { data } = await axios.get("/auth/user");
+    const { data } = await axios.get('/auth/user');
     dispatch(gotUser(data));
     if (data.id) {
-      socket.emit("go-online", data.id);
+      socket.emit('go-online', data.id);
     }
   } catch (error) {
     console.error(error);
@@ -32,36 +32,36 @@ export const fetchUser = () => async (dispatch) => {
   }
 };
 
-export const register = (credentials) => async (dispatch) => {
+export const register = credentials => async dispatch => {
   try {
-    const { data } = await axios.post("/auth/register", credentials);
-    await localStorage.setItem("messenger-token", data.token);
+    const { data } = await axios.post('/auth/register', credentials);
+    await localStorage.setItem('messenger-token', data.token);
     dispatch(gotUser(data));
-    socket.emit("go-online", data.id);
+    socket.emit('go-online', data.id);
   } catch (error) {
     console.error(error);
-    dispatch(gotUser({ error: error.response.data.error || "Server Error" }));
+    dispatch(gotUser({ error: error.response.data.error || 'Server Error' }));
   }
 };
 
-export const login = (credentials) => async (dispatch) => {
+export const login = credentials => async dispatch => {
   try {
-    const { data } = await axios.post("/auth/login", credentials);
-    await localStorage.setItem("messenger-token", data.token);
+    const { data } = await axios.post('/auth/login', credentials);
+    await localStorage.setItem('messenger-token', data.token);
     dispatch(gotUser(data));
-    socket.emit("go-online", data.id);
+    socket.emit('go-online', data.id);
   } catch (error) {
     console.error(error);
-    dispatch(gotUser({ error: error.response.data.error || "Server Error" }));
+    dispatch(gotUser({ error: error.response.data.error || 'Server Error' }));
   }
 };
 
-export const logout = (id) => async (dispatch) => {
+export const logout = id => async dispatch => {
   try {
-    await axios.delete("/auth/logout");
-    await localStorage.removeItem("messenger-token");
+    await axios.delete('/auth/logout');
+    await localStorage.removeItem('messenger-token');
     dispatch(gotUser({}));
-    socket.emit("logout", id);
+    socket.emit('logout', id);
   } catch (error) {
     console.error(error);
   }
@@ -69,22 +69,22 @@ export const logout = (id) => async (dispatch) => {
 
 // CONVERSATIONS THUNK CREATORS
 
-export const fetchConversations = () => async (dispatch) => {
+export const fetchConversations = () => async dispatch => {
   try {
-    const { data } = await axios.get("/api/conversations");
+    const { data } = await axios.get('/api/conversations');
     dispatch(gotConversations(data));
   } catch (error) {
     console.error(error);
   }
 };
 
-const saveMessage = async (body) => {
-  const { data } = await axios.post("/api/messages", body);
+const saveMessage = async body => {
+  const { data } = await axios.post('/api/messages', body);
   return data;
 };
 
 const sendMessage = (data, body) => {
-  socket.emit("new-message", {
+  socket.emit('new-message', {
     message: data.message,
     recipientId: body.recipientId,
     sender: data.sender,
@@ -93,9 +93,9 @@ const sendMessage = (data, body) => {
 
 // message format to send: {recipientId, text, conversationId}
 // conversationId will be set to null if its a brand new conversation
-export const postMessage = (body) => (dispatch) => {
+export const postMessage = body => async dispatch => {
   try {
-    const data = saveMessage(body);
+    const data = await saveMessage(body);
 
     if (!body.conversationId) {
       dispatch(addConversation(body.recipientId, data.message));
@@ -109,7 +109,7 @@ export const postMessage = (body) => (dispatch) => {
   }
 };
 
-export const searchUsers = (searchTerm) => async (dispatch) => {
+export const searchUsers = searchTerm => async dispatch => {
   try {
     const { data } = await axios.get(`/api/users/${searchTerm}`);
     dispatch(setSearchedUsers(data));
