d3655b6d3ec9006ac9e0bf123e34f1282e5c1bb9
Jaehyun Jun <wjswogus0007@gmail.com>
2022-01-26 21:40:40 -0500

    fix: added deep copy of the state recieved to addMessageToStore and add New ConvoToStore reducer

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -1,5 +1,13 @@
 export const addMessageToStore = (state, payload) => {
   const { message, sender } = payload;
+
+  // Editor: Jaehyun Jun
+  // redux compares the memory of the two objects to simply check whether
+  // the previous object is the same as the new object.
+  // Accordingly, when the attributes of the previous object are changed inside the reducer,
+  // both "new state" and "old state" refer to the same object.
+  // Therefore, redux thinks nothing has changed, So I made the deep copy of the state to return.
+  const newState = state.map(convo => Object.assign({}, convo));
   // if sender isn't null, that means the message needs to be put in a brand new convo
   if (sender !== null) {
     const newConvo = {
@@ -8,10 +16,10 @@ export const addMessageToStore = (state, payload) => {
       messages: [message],
     };
     newConvo.latestMessageText = message.text;
-    return [newConvo, ...state];
+    return [newConvo, ...newState];
   }
 
-  return state.map((convo) => {
+  return newState.map(convo => {
     if (convo.id === message.conversationId) {
       convo.messages.push(message);
       convo.latestMessageText = message.text;
@@ -23,7 +31,7 @@ export const addMessageToStore = (state, payload) => {
 };
 
 export const addOnlineUserToStore = (state, id) => {
-  return state.map((convo) => {
+  return state.map(convo => {
     if (convo.otherUser.id === id) {
       const convoCopy = { ...convo };
       convoCopy.otherUser = { ...convoCopy.otherUser, online: true };
@@ -35,7 +43,7 @@ export const addOnlineUserToStore = (state, id) => {
 };
 
 export const removeOfflineUserFromStore = (state, id) => {
-  return state.map((convo) => {
+  return state.map(convo => {
     if (convo.otherUser.id === id) {
       const convoCopy = { ...convo };
       convoCopy.otherUser = { ...convoCopy.otherUser, online: false };
@@ -50,12 +58,12 @@ export const addSearchedUsersToStore = (state, users) => {
   const currentUsers = {};
 
   // make table of current users so we can lookup faster
-  state.forEach((convo) => {
+  state.forEach(convo => {
     currentUsers[convo.otherUser.id] = true;
   });
 
   const newState = [...state];
-  users.forEach((user) => {
+  users.forEach(user => {
     // only create a fake convo if we don't already have a convo with this user
     if (!currentUsers[user.id]) {
       let fakeConvo = { otherUser: user, messages: [] };
@@ -67,7 +75,14 @@ export const addSearchedUsersToStore = (state, users) => {
 };
 
 export const addNewConvoToStore = (state, recipientId, message) => {
-  return state.map((convo) => {
+  // Editor: Jaehyun Jun
+  // redux compares the memory of the two objects to simply check whether
+  // the previous object is the same as the new object.
+  // Accordingly, when the attributes of the previous object are changed inside the reducer,
+  // both "new state" and "old state" refer to the same object.
+  // Therefore, redux thinks nothing has changed, So I made the deep copy of the state to return.
+  const newState = state.map(convo => Object.assign({}, convo));
+  return newState.map(convo => {
     if (convo.otherUser.id === recipientId) {
       convo.id = message.conversationId;
       convo.messages.push(message);
