58c21cf1e28e3957ec54bd1d5f588749966c67b5
Jaehyun Jun <wjswogus0007@gmail.com>
2022-02-04 15:13:28 -0500

    feat: updated findConversation method to search through all participants

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -1,21 +1,18 @@
-const { Op } = require("sequelize");
-const db = require("../db");
-const Message = require("./message");
+const { Op } = require('sequelize');
+const db = require('../db');
+const Message = require('./message');
 
-const Conversation = db.define("conversation", {});
+const Conversation = db.define('conversation', {});
 
 // find conversation given two user Ids
 
-Conversation.findConversation = async function (user1Id, user2Id) {
+Conversation.findConversation = async function (participants) {
   const conversation = await Conversation.findOne({
     where: {
-      user1Id: {
-        [Op.or]: [user1Id, user2Id]
+      userId: {
+        [Op.in]: participants,
       },
-      user2Id: {
-        [Op.or]: [user1Id, user2Id]
-      }
-    }
+    },
   });
 
   // return conversation or null if it doesn't exist
