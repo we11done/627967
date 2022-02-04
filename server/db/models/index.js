7cf1d2eefa056f76f93bb5e7bf23cc191df1c40e
Jaehyun Jun <wjswogus0007@gmail.com>
2022-02-04 15:14:30 -0500

    feat: updated assiociation between conversation and user

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,17 +1,20 @@
-const Conversation = require("./conversation");
-const User = require("./user");
-const Message = require("./message");
+const Conversation = require('./conversation');
+const User = require('./user');
+const Message = require('./message');
 
 // associations
 
 User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+Conversation.belongsToMany(User, {
+  as: 'participants',
+  through: 'participants_of_conversation',
+  foreignKey: 'userId',
+});
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 
 module.exports = {
   User,
   Conversation,
-  Message
+  Message,
 };
