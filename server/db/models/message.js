de44902768b4da14d624d46e279a97e39ed76bf2
Jaehyun Jun <wjswogus0007@gmail.com>
2022-02-04 15:15:37 -0500

    feat: updated Message model to have array of readUsers field and added hook to set readUsers to include senderId on created

--- server/db/models/message.js
+++ server/db/models/message.js
@@ -1,20 +1,28 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Message = db.define('message', {
-  text: {
-    type: Sequelize.STRING,
-    allowNull: false,
+const Message = db.define(
+  'message',
+  {
+    text: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    senderId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    readUsers: {
+      type: Sequelize.ARRAY(Sequelize.INTEGER),
+    },
   },
-  senderId: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  isRead: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-    defaultValue: false,
-  },
-});
+  {
+    hooks: {
+      beforeCreate: (record, options) => {
+        record.dataValues.readUsers = [record.dataValues.senderId];
+      },
+    },
+  }
+);
 
 module.exports = Message;
