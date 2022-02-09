64aff5e863deecc14f75b5823acb005803f7b3e5
Jaehyun Jun <wjswogus0007@gmail.com>
2022-02-08 20:33:02 -0500

    Revert "feat: updated findConversation method to search through all participants" because this is a duplicate This reverts commit b8099a5ceb2c4864daae02c971a0e9a42e788490.

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -1,18 +1,21 @@
-const { Op } = require('sequelize');
-const db = require('../db');
-const Message = require('./message');
+const { Op } = require("sequelize");
+const db = require("../db");
+const Message = require("./message");
 
-const Conversation = db.define('conversation', {});
+const Conversation = db.define("conversation", {});
 
 // find conversation given two user Ids
 
-Conversation.findConversation = async function (participants) {
+Conversation.findConversation = async function (user1Id, user2Id) {
   const conversation = await Conversation.findOne({
     where: {
-      userId: {
-        [Op.in]: participants,
+      user1Id: {
+        [Op.or]: [user1Id, user2Id]
       },
-    },
+      user2Id: {
+        [Op.or]: [user1Id, user2Id]
+      }
+    }
   });
 
   // return conversation or null if it doesn't exist
